024325498d24d066bfb897379bfd0499822d6c72
AD <alexanderdjordjevic11@gmail.com>
2022-04-18 20:25:28 -0400

    Added sample animation. Reformatted keyboard.

--- Main/Resources/PHL_1.js
+++ Main/Resources/PHL_1.js
@@ -20,12 +20,7 @@ var speed = 1500; //NOTE: Speed (variable shown right above refreshInterval() fu
 			var newHeader = textDecommand.substring(textDecommand.indexOf(' ')+1);
 			var oldHeader = textDecommand.substring(0,textDecommand.indexOf(' '));
 			var oldInner = document.getElementById(oldHeader).innerHTML;
-			document.getElementById(oldHeader).innerHTML
-                =  newHeader +" "+ oldInner.substring(oldInner.indexOf(' ')+1);
-			
-			
-            
-			 
+			document.getElementById(oldHeader).innerHTML =  newHeader +" "+ oldInner.substring(oldInner.indexOf(' ')+1);
  
 	}
 
@@ -36,33 +31,27 @@ function sleep(ms) {
 }
 
 
-
  function myMove() { //change 
   var elem = document.getElementById("animate");  //change  
   var pos = 76; //where element starts
-   function movediv(){
-    if (pos == 476){ //where element heads to
+
+   function movediv()
+   {
+    if (pos == 476) { //where element heads to
       document.getElementById('animate').style.display='none';  //change. hides element once animation is completed: https://www.tutorialspoint.com/how-to-hide-show-html-elements-in-javascript
-    
-} else {
-	
+    } 
+    else 
+    {
       pos+=10;
        elem.style.top = pos + "px"; 
        elem.style.left = pos + "px"; ; 
-    
-	 sleep(speed).then(() => {
-	  requestAnimationFrame(movediv)
 
-	});
-
-	
- 
-
+	  sleep(speed).then(() => {requestAnimationFrame(movediv)});
     }
- 
   }	
   requestAnimationFrame(movediv)
 }
+
 // z-index: 1; left: 50px; top: 280px; width: 11px; height: 12px;
 var id2=null;
  function myMove2() { //change 
@@ -83,6 +72,27 @@ var id2=null;
 
     }
  
+  }	
+  requestAnimationFrame(movediv)
+}
+
+function myMove3() { //change 
+  var elem = document.getElementById("animate3");  //change  
+  var pos3 = 421; //where element starts
+
+   function movediv()
+   {
+    if (pos3 == 531) { //where element heads to
+      document.getElementById('animate3').style.display='none';  //change. hides element once animation is completed: https://www.tutorialspoint.com/how-to-hide-show-html-elements-in-javascript
+    } 
+    else 
+    {
+      pos3+=10;
+       elem.style.top = pos3 + "px"; 
+       elem.style.left = pos3 + "px"; ; 
+
+	  sleep(speed).then(() => {requestAnimationFrame(movediv)});
+    }
   }	
   requestAnimationFrame(movediv)
 }
